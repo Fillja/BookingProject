349f52e43a86b86e4a70b684f276a816ca47965b
Robin Gombkötö <g_gomben_love@hotmail.com>
2024-11-26 11:35:56 +0100

    created context & responseresult

---
+++ Infrastructure/Helpers/ResponseResult.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Helpers;
+
+
+public enum StatusCodes
+{
+    OK = 200,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    NOT_FOUND = 404,
+    CONFLICT = 409
+}
+
+public class ResponseResult
+{
+    public StatusCodes StatusCode { get; set; }
+    public object? Content { get; set; }
+    public string? Message { get; set; }
+}
